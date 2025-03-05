3843e5905c9710c5a04a6b855941aa21d2a18c39
artemmolodoy1487 <myanowitch@yandex.ru>
2025-03-05 13:31:14 +0300

    Add simple models object()->file||dir

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
-
-struct FileSystemObject {
-    
-};
-
-struct FileObject {
-
+trait FileSystemObject: std::fmt::Debug {
+    fn get_name(&self) -> &str;
 }
 
-struct DirObject {
+#[derive(Debug)]
+struct FileObject {
+    name: String,
+    content: String,
+}
 
+#[derive(Debug)]
+struct DirObject {
+    name: String,
+    content: Vec<Box<dyn FileSystemObject>>,
+}
+
+impl FileSystemObject for FileObject {
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+}
+
+impl FileSystemObject for DirObject {
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+}
+
+impl FileObject {
+    fn get_content(&self) -> &str {
+        &self.content // Возвращаем содержимое файла
+    }
+}
+
+impl DirObject {
+    fn get_content(&self) -> &Vec<Box<dyn FileSystemObject>> {
+        &self.content // Возвращаем ссылку на содержимое директории
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    
 }
+
+// Добавляем трейт для приведения типа
